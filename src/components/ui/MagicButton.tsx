fbb82f4bf89a1bf9c0b37f2a4e4d37930e1b14bd
dangtinh <caodangtinh04022003@gmail.com>
2024-07-06 16:52:56 +0700

    add: hero section

---
+++ src/components/ui/MagicButton.tsx
@@ -0,0 +1,19 @@
+import React, { HTMLAttributes } from "react";
+
+interface MagicButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode;
+}
+
+const MagicButton = ({ className, icon, ...props }: MagicButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={`inline-flex gap-x-3 py-2 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${className}`}
+    >
+      {props.children}
+      {icon}
+    </button>
+  );
+};
+
+export default MagicButton;
