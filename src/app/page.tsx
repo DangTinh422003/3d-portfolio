69f371925d5584b9ff6b136fe27bf596cd96cd77
dangtinh <caodangtinh04022003@gmail.com>
2024-07-18 14:20:34 +0700

    update: rm navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <TracingBeam>
-          <FloatingNavbar navItems={navItems} />
+          {/* <FloatingNavbar navItems={navItems} /> */}
           <Hero />
           <Grid />
           <Projects />
